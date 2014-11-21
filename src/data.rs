74d39b1db98f857bb88334a96aa348b768e20b9b
Johannes Dollinger <johannes.dollinger@deliveryhero.com>
2014-11-21 17:40:28 +0100

    Introduced a DataStore and FileStore

--- src/data.rs
+++ src/data.rs
@@ -6,14 +6,38 @@ use rust_crypto::digest::Digest;
 
 static root_path: &'static str = "_documents";
 
-pub fn write_data(data: &[u8]){
-  let mut hash = Md5::new();
-  hash.input(data);
-  let mut hashbits: [u8, ..16] = [0u8, ..16];
-  hash.result(&mut hashbits);
-  let root: Path = Path::new(&root_path);
-  let path = root.join(hashbits.as_slice().to_hex());
 
-  let mut file = File::create(&path);
-  file.write(data);
+pub trait DataStore {
+  fn write(&self, data: &[u8]) -> ();
 }
+
+
+pub struct FileStore{
+  path: String,
+}
+
+
+impl FileStore{
+  pub fn new(path: String) -> FileStore {
+    FileStore {
+      path: path,
+    }
+  } 
+}
+
+
+impl DataStore for FileStore{
+  fn write(&self, data: &[u8]){
+    let mut hash = Md5::new();
+    hash.input(data);
+    let mut hashbits: [u8, ..16] = [0u8, ..16];
+    hash.result(&mut hashbits);
+    let root: Path = Path::new(&root_path);
+    let path = root.join(hashbits.as_slice().to_hex());
+
+    let mut file = File::create(&path);
+    file.write(data);
+  }
+}
+
+
