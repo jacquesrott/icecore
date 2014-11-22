ea899ede4f94b3bb3eb1c6f884080beecf7232b2
Johannes Dollinger <emulbreh@googlemail.com>
2014-11-22 15:40:09 +0100

    Added DataStore.read()

--- src/data.rs
+++ src/data.rs
@@ -5,36 +5,49 @@ use rust_crypto::digest::Digest;
 
 
 pub trait DataStore {
-  fn write(&self, data: &[u8]) -> ();
+  fn write(&self, data: &[u8]) -> String;
+  fn read(&self, hash: &String) -> Vec<u8>;
 }
 
 
 pub struct FileStore{
-  path: String,
+    path: Path,
 }
 
 
 impl FileStore{
-  pub fn new(path: String) -> FileStore {
-    FileStore {
-      path: path,
+    pub fn new(path: String) -> FileStore {
+        FileStore {
+            path: Path::new(path.as_slice()),
+        }
     }
-  } 
+
+    fn get_path(&self, hash: &String) -> Path {
+        self.path.clone().join(hash)
+    }
 }
 
 
 impl DataStore for FileStore{
-  fn write(&self, data: &[u8]){
-    let mut hash = Md5::new();
-    hash.input(data);
-    let mut hashbits: [u8, ..16] = [0u8, ..16];
-    hash.result(&mut hashbits);
-    let root: Path = Path::new(self.path.as_slice());
-    let path = root.join(hashbits.as_slice().to_hex());
+    fn write(&self, data: &[u8]) -> String{
+        println!("write {}", data);
+        let mut h = Md5::new();
+        h.input(data);
+        let mut hashbits: [u8, ..16] = [0u8, ..16];
+        h.result(&mut hashbits);
+        let data_hash = hashbits.as_slice().to_hex();
+        let path = self.get_path(&data_hash);
 
-    let mut file = File::create(&path);
-    file.write(data);
-  }
+        let mut file = File::create(&path);
+        file.write(data);
+        data_hash
+    }
+
+    fn read(&self, hash: &String) -> Vec<u8>{
+        println!("read {}", hash);
+        let path = self.get_path(hash);
+        File::open(&path).read_to_end().unwrap()
+    }
 }
 
 
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ use icecore::data::DataStore;
 
 
 fn main() {
-  let foo = "bar";
-  let store = FileStore::new(String::from_str("_documents"));
-  store.write(foo.as_bytes());
+    let foo = "bar";
+    let store = FileStore::new(String::from_str("_documents"));
+    let hash = store.write(foo.as_bytes());
+    println!("stored as {}", hash);
+    println!("data: {}", store.read(&hash));
 }
