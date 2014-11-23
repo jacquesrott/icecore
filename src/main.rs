a263580ded2701036c9b26d6d88f9ec7aa931d5f
Johannes Dollinger <emulbreh@googlemail.com>
2014-11-23 18:30:10 +0100

    Added update command

--- src/main.rs
+++ src/main.rs
@@ -32,22 +32,20 @@ fn main() {
 
     ic.load();
 
+    if args.len() < 2 {
+        panic!("not enough arguments");
+    }
+
     match args[1].as_slice() {
         "insert" => {
             let opts = [
-                optopt("d", "data", "Data to insert into the database", "DATA"),
+                optopt("d", "data", "Data to write", "DATA"),
                 optflag("h", "help", "print this help message and exit"),
             ];
-
-            let matches = match getopts(args.tail(), &opts) {
+            let matches = match getopts(args.slice_from(1), &opts) {
                 Ok(m) => m,
-                Err(_) => panic!("{}", usage("Insert data in the data store", &opts)),
+                Err(_) => panic!(usage("Insert new document", &opts)),
             };
-
-            if matches.opt_present("h") {
-                println!("{}", usage("Insert data in the data store", &opts));
-                return;
-            }
 
             let data = matches.opt_str("d").unwrap();
             ic.insert(data);
@@ -60,6 +58,7 @@ fn main() {
                 Ok(m) => m,
                 Err(_) => panic!(usage("Get document by id", &opts)),
             };
+
             let id = from_str::<u64>(args[2].as_slice()).unwrap();
             let version: Option<u64> = match matches.opt_str("v") {
                 Some(v) => Some(from_str::<u64>(v.as_slice()).unwrap()),
@@ -68,6 +67,20 @@ fn main() {
             println!("getting document id={} version={}", id, version);
             let data = ic.get(id, version);
             println!("{}", data);
+        },
+        "update" => {
+            let opts = [
+                optopt("d", "data", "Data to write", "DATA"),
+            ];
+            let matches = match getopts(args.slice_from(2), &opts) {
+                Ok(m) => m,
+                Err(_) => panic!(usage("", &opts)),
+            };
+
+            let id = from_str::<u64>(args[2].as_slice()).unwrap();
+            let data = matches.opt_str("d").unwrap();
+            println!("updating document id={}", id);
+            ic.update(id, data);
         },
         "demo" => {
             demo(&mut ic);
