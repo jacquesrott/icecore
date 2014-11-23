f97572ba8794f36e890cc9eb1d6400a6e9ad11c0
Johannes Dollinger <emulbreh@googlemail.com>
2014-11-23 16:00:41 +0100

    Factored out DocumentIndex

--- src/ic.rs
+++ src/ic.rs
@@ -18,10 +18,46 @@ pub struct VersionIndex{
 }
 
 
+pub struct DocumentIndex {
+    documents: Vec<VersionIndex>,
+}
+
+
+impl DocumentIndex {
+    fn new() -> DocumentIndex{
+        DocumentIndex {
+            documents: Vec::new(),
+        }
+    }
+
+    fn get_version_index(&self, id: u64) -> &VersionIndex {
+        self.documents.index(&(id - 1).to_uint().unwrap())
+    }
+
+    fn get_version_index_mut(&mut self, id: u64) -> &mut VersionIndex {
+        self.documents.index_mut(&(id - 1).to_uint().unwrap())
+    }
+
+    fn get_next_id(&self) -> u64 {
+        self.documents.len().to_u64().expect("document id overflow") + 1
+    }
+
+    fn add(&mut self, doc: Document) {
+        let mut index = VersionIndex {
+            document_id: doc.id,
+            versions: Vec::new(),
+        };
+        index.versions.push(doc);
+        self.documents.push(index);
+    }
+
+}
+
+
 pub struct Icecore<'a>{
     store: Box<DataStore + 'a>,
     transaction_log: Vec<Transaction>,
-    document_index: Vec<VersionIndex>,
+    document_index: DocumentIndex,
 }
 
 
@@ -30,46 +66,29 @@ impl<'a> Icecore<'a>{
         Icecore {
             store: data_store,
             transaction_log: Vec::new(),
-            document_index: Vec::new(),
+            document_index: DocumentIndex::new(),
         }
-    }
-
-    fn get_next_document_id(&self) -> u64 {
-        self.document_index.len().to_u64().expect("document id overflow") + 1
     }
 
     fn get_next_transaction_id(&self) -> u64 {
         self.transaction_log.len().to_u64().expect("transaction id overflow") + 1
     }
 
-    fn get_version_index(&self, id: u64) -> &VersionIndex {
-        self.document_index.index(&(id - 1).to_uint().unwrap())
-    }
-
-    fn get_version_index_mut(&mut self, id: u64) -> &mut VersionIndex {
-        self.document_index.index_mut(&(id - 1).to_uint().unwrap())
-    }
-
     pub fn insert(&mut self, data: String) -> u64 {
         let hash = self.store.write(data);
-        let id = self.get_next_document_id();
+        let id = self.document_index.get_next_id();
         let doc = Document {
             id: id,
             version: self.get_next_transaction_id(),
             hash: hash,
         };
-        let mut index = VersionIndex {
-            document_id: doc.id,
-            versions: Vec::new(),
-        };
-        index.versions.push(doc);
-        self.document_index.push(index);
+        self.document_index.add(doc);
 
         id
     }
 
     pub fn get(&self, id: u64, version: Option<u64>) -> String {
-        let index = self.get_version_index(id);
+        let index = self.document_index.get_version_index(id);
         let doc = match version {
             Some(v) => &index.versions[v.to_uint().unwrap() - 1],
             None => index.versions.last().expect("no document versions found"),
@@ -85,7 +104,7 @@ impl<'a> Icecore<'a>{
             version: version,
             hash: hash,
         };
-        let mut index = self.get_version_index_mut(id);
+        let mut index = self.document_index.get_version_index_mut(id);
         index.versions.push(doc);
 
         version
