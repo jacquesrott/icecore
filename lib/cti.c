be8d4863836cd5c053cae5c740fc32f68321c381
Johannes Dollinger <johannes.dollinger@deliveryhero.com>
2014-11-28 16:52:14 +0100

    Use cti from rust

---
+++ lib/cti.c
@@ -0,0 +1,256 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+#include "cti.h"
+
+
+LeafNode* leafnode_create(Version version){
+    LeafNode* leaf = malloc(sizeof(*leaf));
+    leaf->version = version;
+    for (unsigned int i = 0; i < LEAF_NODE_SIZE; i++) {
+        leaf->values[i] = NONE;
+    }
+    return leaf;
+}
+
+LeafNode* leafnode_copy(LeafNode* base, Version version){
+    LeafNode* leaf = malloc(sizeof(*leaf));
+    leaf->version = version;
+    for (unsigned int i = 0; i < LEAF_NODE_SIZE; i++) {
+        leaf->values[i] = base->values[i];
+    }
+    return leaf;
+}
+
+IndexNode* indexnode_create(Version version){
+    IndexNode* node = malloc(sizeof(*node));
+    node->version = version;
+    node->children[0] = NULL;
+    node->children[1] = NULL;
+    return node;
+}
+
+IndexNode* indexnode_copy(IndexNode* base, Version version){
+    IndexNode* node = malloc(sizeof(*node));
+    node->version = version;
+    node->children[0] = base->children[0];
+    node->children[1] = base->children[1];
+    return node;
+}
+
+
+void node_delete(IndexNode* node, Version version, unsigned int depth) {
+    if (node == NULL || node->version != version) {
+        return;
+    }
+    if (depth != 0) {
+        node_delete(node->children[0], version, depth - 1);
+        node_delete(node->children[1], version, depth - 1);
+        
+    }
+    free(node);
+}
+
+size_t node_size(IndexNode* node, Version version, unsigned int depth) {
+    if(node == NULL || node->version != version){
+        return 0;
+    }
+    if (depth == 0) {
+        return sizeof(LeafNode);
+    }
+    size_t size = sizeof(*node);
+    size += node_size(node->children[0], version, depth - 1);
+    size += node_size(node->children[1], version, depth - 1);
+    return size;
+}
+
+void node_dump(IndexNode* node, unsigned int depth, unsigned int indent){
+    for(int i=0; i< indent; i++){
+        printf("    ");
+    }
+    if(node == NULL){
+        printf("NULL\n");
+        return;
+    }
+    if(depth == 0){
+        LeafNode* leaf = (LeafNode*)node;
+        printf("<%p> ", leaf);
+        for(int i=0;i<LEAF_NODE_SIZE;i++){
+            printf("%llu, ", leaf->values[i]);
+        }
+        printf("\n");
+    }
+    else{
+        printf("<%p> version=%llu\n", node, node->version);
+        node_dump(node->children[0], depth - 1, indent + 1);
+        node_dump(node->children[1], depth - 1, indent + 1);
+    }
+}
+
+
+Tree* tree_create(Version version, Tree* base) {
+    Tree* tree = malloc(sizeof(*tree));
+    tree->version = version;
+    IndexNode* root = indexnode_create(version);
+    
+    if (base != NULL) {
+        root->children[0] = base->root->children[0];
+        root->children[1] = base->root->children[1];
+        tree->depth = base->depth;
+    }
+    else{
+        tree->depth = 1;
+    }
+
+    tree->root = root;
+    return tree;
+}
+
+size_t tree_capacity(Tree* tree){
+    return LEAF_NODE_SIZE * (1 << tree->depth);
+}
+
+void tree_insert(Tree* tree, Id id, Value value) {
+    size_t capacity = tree_capacity(tree);
+    while(id >= capacity){
+        IndexNode* new_root = indexnode_create(tree->version);
+        new_root->children[0] = tree->root;
+        tree->root = new_root;
+        tree->depth += 1;
+        capacity *= 2;
+    }
+    
+    Id x = id / LEAF_NODE_SIZE;
+    IndexNode* node = tree->root;
+
+    for(int i = tree->depth - 1; i >= 0; i--) {
+        Version child_index = (x & (1 << i)) >> i;
+        IndexNode* child = node->children[child_index];
+        if (child == NULL){
+            if (i == 0) {
+                child = (IndexNode*)leafnode_create(tree->version);
+            }
+            else {
+                child = indexnode_create(tree->version);
+            }
+            node->children[child_index] = child;
+        }
+        if (child->version != tree->version) {
+            if (i == 0){
+                child = (IndexNode*)leafnode_copy((LeafNode*)child, tree->version);
+            }
+            else{
+                child = indexnode_copy(child, tree->version);;
+            }
+            node->children[child_index] = child;
+        }
+        node = child;
+    }
+    printf("insert (depth=%u, index=%llu)\n", tree->depth, id % LEAF_NODE_SIZE);
+    LeafNode* leaf = (LeafNode*) node;
+    leaf->values[id % LEAF_NODE_SIZE] = value;
+}
+
+int tree_get(Tree* tree, Id id, Value* value){
+    if (id > tree_capacity(tree)){
+        return NONE;
+    }
+    IndexNode* node = tree->root;
+    Id x = id / LEAF_NODE_SIZE;
+    for(int i = tree->depth - 1; i >= 0; i--) {
+        Version child_index = (x & (1 << i)) >> i;
+        IndexNode* child = node->children[child_index];
+        if (child == NULL){
+            return DOCUMENT_DOES_NOT_EXIST;
+        }
+        node = child;
+    }
+    LeafNode* leaf = (LeafNode*) node;
+    *value = leaf->values[id % LEAF_NODE_SIZE];
+    return OK;
+}
+
+void tree_delete(Tree* tree) {
+    node_delete(tree->root, tree->version, tree->depth);
+    free(tree);
+}
+
+size_t tree_size(Tree* tree) {
+    return sizeof(*tree) + node_size(tree->root, tree->version, tree->depth);
+}
+
+void tree_dump(Tree* tree){
+    node_dump(tree->root, tree->depth, 0);
+}
+
+
+Index* index_create() {
+    Index* idx = malloc(sizeof(*idx));
+    idx->trees = calloc(MAX_VERSIONS, sizeof(*idx->trees));
+    idx->trees[0] = tree_create(0, NULL);
+    idx->last_version = 0;
+    return idx;
+}
+
+int index_insert(Index* idx, Id id, Version version, Value value) {
+    assert(version > 0);
+    if (version > idx->last_version + 1) {
+        return VERSION_SKIPPED;
+    }
+    Tree* tree = idx->trees[version];
+    if (tree == NULL) {
+        tree = tree_create(version, idx->trees[version - 1]);
+        idx->trees[version] = tree;
+    }
+    tree_insert(tree, id, value);
+    idx->last_version = version;
+    return 0;
+}
+
+
+int index_get(Index* idx, Id id, Version version, Value* value){
+    if (version > idx->last_version) {
+        return VERSION_DOES_NOT_EXIST;
+    }
+    Tree* tree = idx->trees[version];
+    if (tree == NULL){
+        return VERSION_DOES_NOT_EXIST;
+    }
+    return tree_get(tree, id, value);
+}
+
+size_t index_size(Index* idx){
+    size_t size = sizeof(*idx) + idx->last_version * sizeof(*idx->trees);
+    for (int i = idx->last_version; i >= 0; i--) {
+        size += tree_size(idx->trees[i]);
+    }
+    return size;
+}
+
+void index_delete(Index* idx) {
+    for (int i = idx->last_version; i >= 0; i--) {
+        tree_delete(idx->trees[i]);
+    }
+    free(idx->trees);
+    free(idx);
+}
+
+
+int main(int argc, char** argv) {
+    Index* idx = index_create();
+    for(int i=1;i<50;i++){
+        index_insert(idx, 2*i, i, i);
+        index_insert(idx, 2*i+1, i, i);
+        index_insert(idx, i, i, 2*i);
+    }
+
+    for(int i=1;i<50;i++){
+        Value val;
+        int error = index_get(idx, i, 20, &val);
+        printf("%i: %llu (error=%i)\n", i, val, error);
+    }
+    tree_dump(idx->trees[40]);
+    printf("index size = %zu bytes\n", index_size(idx));
+    index_delete(idx);
+}
