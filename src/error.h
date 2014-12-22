abd922c1d8e209778c32bd549372966f3658d5aa
Jacques Rott <jacques.rott@gmail.com>
2014-12-23 00:13:50 +0100

    Add macros for error handling

--- src/error.h
+++ src/error.h
@@ -1,6 +1,8 @@
 #ifndef IC_ERROR_H
 #define IC_ERROR_H
 
+#include <stdio.h>
+
 typedef enum {
     ICE_OK = 0,
     ICE_DOCUMENT_DOES_NOT_EXIST,
@@ -10,5 +12,12 @@ typedef enum {
 } ice_t;
 
 const char* error_string(ice_t code);
+
+#define check(e) if(e == NULL) goto error;
+#define check_negative(e) if(e == -1) goto error;
+#define check_ok(e) if(e != ICE_OK) goto error;
+
+#define pcheck(e, s) if(e == NULL) { perror(s); goto error; }
+#define pcheck_negative(e, s) if(e == -1) { perror(s); goto error; }
 
 #endif
